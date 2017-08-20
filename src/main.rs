07401a05d82245f7f2b549150893618548f7176b
Jan Schulte <hello@unexpected-co.de>
2017-08-20 22:16:58 +0200

    feat: install clap to provide a basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("diagnostic")
+                    .version("1.0")
+                    .author("Jan Schulte. <schulte@unexpected-co.de>")
+                    .about("Provides insights about your log files")
+                    .arg(Arg::with_name("working_directory")
+                            .short("w")
+                            .long("working_directory")
+                            .value_name("DIRECTORY")
+                            .help("specifies working directory")
+                            .takes_value(true))
+                    .get_matches();
+    let working_directory = matches.value_of("working_directory").unwrap_or("./");
+    println!("Value for config: {}", working_directory);
 }
